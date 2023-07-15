6a2ef904d3b2b390c77fe081db96b11c3d6e4d48
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-07-15 23:44:19 +0300

    added two models schemas

--- app/internal/insurance/schemas.py
+++ app/internal/insurance/schemas.py
@@ -1,6 +1,6 @@
 import datetime
 from decimal import Decimal
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from app.database import CargoTypes
 
 
@@ -12,4 +12,14 @@ class CalculateRequest(BaseModel):
 
 class CalculateResponse(BaseModel):
     insurance_value: Decimal
+
+
+class InsuranceResponse(BaseModel):
+    cargo_type: str = Field(..., example='Glass')
+    rate: Decimal = Field(..., example=0.04)
+    date: datetime.date = Field(..., example=datetime.date(2023, 1, 1))
+
+
+class InsuranceResponseList(BaseModel):
+    cargo_types: list[InsuranceResponse]
 
