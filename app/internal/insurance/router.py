56df2141a2afa3f475e3473491685854804ec6d8
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-07-15 23:45:44 +0300

    converted endpoints to a class

--- app/internal/insurance/router.py
+++ app/internal/insurance/router.py
@@ -1,12 +1,26 @@
 from fastapi import APIRouter
-from .schemas import CalculateRequest, CalculateResponse
-from .services import calculate_insurance
+from .schemas import CalculateRequest, CalculateResponse,\
+    InsuranceResponseList, InsuranceResponse
+from .services import Service
 
 
 router = APIRouter(prefix='/api/v1/insurance')
 
 
-@router.post('/calculate', response_model=CalculateResponse)
-async def calculate(data: CalculateRequest) -> CalculateResponse:
-    insurance = await calculate_insurance(data)
-    return CalculateResponse(insurance_value=insurance)
+class InsuranceService:
+
+    @staticmethod
+    @router.post('/calculate', response_model=CalculateResponse)
+    async def calculate(data: CalculateRequest) -> CalculateResponse:
+        insurance = await Service.calculate_insurance(data)
+        return CalculateResponse(insurance_value=insurance)
+
+    @staticmethod
+    @router.get('/all_by_cargo_type', response_model=InsuranceResponseList)
+    async def all_by_cargo_type(cargo_type: str) -> InsuranceResponseList:
+        cargo_types = await Service.all_by_cargo_type(cargo_type)
+        return InsuranceResponseList(cargo_types=[InsuranceResponse(
+            cargo_type=item.cargo_type,
+            rate=item.rate,
+            date=item.date
+        ) for item in cargo_types])
