14256746d02f7b50ecf2b88914721251f99e2d94
KLYMENKORUS <klimenkoruslan11@gmail.com>
2023-07-16 20:08:24 +0300

    deleted db_init.py

--- app/database/db_init.py
+++
@@ -1,9 +0,0 @@
-from tortoise import Tortoise
-from app.configuration import settings
-
-
-class DBInitial:
-
-    @classmethod
-    async def init_db(cls) -> None:
-        await Tortoise.init(config=settings.DB_CONFIG)
